b732a5689d3b85dc8bbac968da35f918728f8272
zfkun <zfkun@msn.com>
2013-11-01 17:17:27 +0800

    complete api & update demo
    complete apis
    update demo & package & css

--- src/tab.js
+++ src/tab.js
@@ -24,9 +24,10 @@ define(function ( require ) {
      * @requires lang
      * @requires dom
      * @requires Control
-     * @fires module:Tab#click
+     * @fires module:Tab#add
+     * @fires module:Tab#remove
      * @fires module:Tab#change
-     * @fires module:Tab#close
+     * @fires module:Tab#click
      */
     var Tab = function() {
         Control.apply( this, arguments );
@@ -125,6 +126,7 @@ define(function ( require ) {
          */
         initStructure: function () {
             var trigger = this.trigger;
+            var needRebuild = !trigger;
             var selectedIndex = this.selectedIndex;
 
             // 到这里已经没用了
@@ -133,34 +135,27 @@ define(function ( require ) {
 
             // 若`init`执行后，不存在`trigger`变量，
             // 则需要动态构建必要结构`ul > li * n`
-            if ( !trigger ) {
+            // 这里先构建总容器元素`ul`
+            if ( needRebuild ) {
                 trigger = document.createElement( 'ul' );
                 this.main.insertBefore(
                     trigger,
                     this.main.firstChild || null
                 );
-
-                // 根据用户传入配置`options`.`tabs`
-                // 自动创建所有tab
-                var triggers = [];
-                this.tabs.forEach(function ( tabItem, i ) {
-                    var li = createTabElement(
-                        this,
-                        tabItem,
-                        i === selectedIndex
-                    );
-                    triggers.push( li );
-                    trigger.appendChild( li );
-                }, this);
-
-                // 存储选项卡对应元素，减少后续的dom查询次数
-                this.triggers = triggers;
             }
 
             // 初始化`navigator`元素相关属性、样式及事件
             trigger.id = helper.getId( this, 'navigator' );
             helper.addPartClasses( this, 'navigator', trigger );
             this.addDOMEvent( trigger, 'click', lang.bind( clickTab, this) );
+
+            // 动态构建所有tab元素
+            // 这行不能提前到上面的三行之前执行
+            // 因 `rebuildTabs` 内部用到了 `getId` 查找页卡容器
+            // 所以，需要等上面三行初始化完成后才行
+            if ( needRebuild ) {
+                rebuildTabs( this );
+            }
 
             // 激活默认项
             activateTab( this, this.activeIndex );
@@ -185,9 +180,79 @@ define(function ( require ) {
          * @param {Object=} changes 变更过的属性的集合
          */
         repaint: function ( changes ) {
+            if ( changes && changes.hasOwnProperty( 'tabs' ) ) {
+                rebuildTabs( this, changes.tabs );
+            }
+
             if ( changes && changes.hasOwnProperty( 'activeIndex' ) ) {
                 activateTab( this, changes.activeIndex.newValue );
             }
+        },
+
+        /**
+         * 批量设置控件的属性值
+         * 
+         * @override
+         * @param {Object} properties 属性值集合
+         */
+        setProperties: function ( properties ) {
+            // 控件禁用状态下，不响应数据更新类操作
+            // 注意:
+            // 这里直接使用了`delete`，外部调用时小心使用
+            // 若不希望传入的参数被更改，最好传入副本进来
+            // TODO: 找时间考虑换种方式
+            if ( this.disabled ) {
+                delete properties.tabs;
+                delete properties.activeIndex;
+            }
+
+            // 如果`tabs`配置变了，则会导致`navigator`整个重新渲染，
+            // 则后续的`repaint`处理`activeIndex`会产生一次浪费，
+            // 因此这里直接把`activeIndex`放自己身上，等`navigator`渲染后使用，
+            // 不再把`activeIndex`加入到`changes`集合中去
+            if ( properties.tabs ) {
+                if ( !properties.hasOwnProperty( 'activeIndex' ) ) {
+                    // 如果仅改变`tabs`，则由于标签页的数量变化，
+                    // 确认现在激活的标签页是不是还在新的`tabs`中，
+                    // 如果不在了，则激活第1个标签
+                    var currentActiveTab = this.tabs[ this.activeIndex ];
+                    var activeIndex = -1;
+                    properties.tabs.some(function ( tabItem, i ) {
+                        if ( tabItem === currentActiveTab ) {
+                            activeIndex = i;
+                            return true;
+                        }
+                    });
+
+                    // 只有当激活的元素变了的时候，才需要触发`activate`事件，
+                    // 事件的触发由`properties`里有没有`activeIndex`决定，
+                    // 因此如果新的`tabs`中没有原来激活的那个标签，则要触发一下
+                    if ( activeIndex === -1 ) {
+                        // 为了让基类`setProperties`检测到变化，
+                        // 先把`this.activeIndex`改掉
+                        this.activeIndex = -1;
+                        properties.activeIndex = 0;
+                    }
+                    else {
+                        this.activeIndex = activeIndex;
+                    }
+                }
+            }
+
+            // `properties`里设置了`activeIndex`
+            // 则要调用检测方法判定是否允许改变
+            // 若检测返回`false`，则不做更新并移除`activeIndex`项
+            if ( properties.hasOwnProperty( 'activeIndex' ) ) {
+                var allowChange = this.beforeChange(
+                    this.activeIndex,
+                    properties.activeIndex
+                );
+                if ( allowChange === false ) {
+                    delete properties.activeIndex;
+                }
+            }
+
+            Control.prototype.setProperties.call( this, properties );
         },
 
         /**
@@ -199,24 +264,162 @@ define(function ( require ) {
          * @param {number} newIndex 新选中项索引值
          * @return {boolean} 是否执行切换
          */
-        onBeforeChange: function ( oldIndex, newIndex ) {
+        beforeChange: function ( oldIndex, newIndex ) {
             return true;
         },
 
+        /**
+         * 添加一个标签页
+         * 
+         * @public
+         * @param {Object} tabItem 标签页的配置
+         * @param {string} tabItem.title 标签页的标题
+         * @param {string=} tabItem.panel 标签页对应的容器的id
+         */
         add: function ( tabItem ) {
-            // TODO
+            this.insert( tabItem, this.tabs.length );
         },
 
+        /**
+         * 在指定位置添加一个标签页
+         * 
+         * @public
+         * @param {Object} tabItem 标签页的配置
+         * @param {string} tabItem.title 标签页的标题
+         * @param {string=} tabItem.panel 标签页对应的容器的id
+         * @param {number} index 新标签页要插入的位置索引
+         */
         insert: function( tabItem, index ) {
-            // TODO
+            if ( this.isDisabled() ) {
+                return;
+            }
+
+            index = Math.max( Math.min( index, this.tabs.length ), 0 );
+
+            this.tabs.splice( index, 0, tabItem );
+
+            var trigger = dom.g( helper.getId( this, 'navigator' ) );
+            var tabElement = createTabElement( this, tabItem, false );
+            trigger.insertBefore(
+                tabElement,
+                trigger.children[ index ] || null
+            );
+
+            // 由于实例存储着一份`tabs`对应的DOM元素集合
+            // 这里也需要同步插入更新
+            this.triggers.splice( index, 0, tabElement );
+
+            // 如果原来是没有标签页的，则新加的这个默认激活
+            if ( this.tabs.length === 1 ) {
+                this.activeIndex = 0;
+                activateTab( this, 0, -1 );
+            }
+            else {
+                // 如果在当前激活的标签前面插入一个，则`activeIndex`需要变化，
+                // 但视图是不用刷新的
+                if ( index <= this.activeIndex ) {
+                    this.activeIndex++;
+                }
+
+                // 新加入的标签默认要隐藏起来
+                if ( tabItem.panel ) {
+                    dom.hide( dom.g( tabItem.panel ) );
+                }
+            }
+
+            /**
+             * @event module:Tab#add
+             * @param {Object} ev 事件参数对象
+             * @param {string} ev.type 事件类型
+             * @param {Tab} ev.target 触发事件的控件对象
+             * @param {Object} data 当前激活的标签信息
+             * @param {number} data.index 标签索引
+             * @param {Object} data.tab 标签数据项
+             * @param {string} data.tab.title 标签显示标题
+             * @param {string=} data.tab.panel 标签关联的dom容器id
+             */
+            this.emit( 'add', { index: index, tab: tabItem } );
         },
 
+        /**
+         * 移除一个标签页
+         * 
+         * @public
+         * @param {Object} tabItem 标签页的配置
+         */
         remove: function ( tabItem ) {
-            // TODO
+            if ( this.isDisabled() ) {
+                return;
+            }
+
+            // 这里使用`while`是解决`this.tabs`在嵌套循环里被修改带来的问题
+            // 若这里使用`for`循环来处理，则会因为`removeByIndex`里
+            // 直接修改了`this.tabs`(`slice`)而发生各种定位错误
+            // 此坑很隐晦，修改需谨慎
+            // TODO: 每次都`indexOf`，性能上貌似一般，待想到好方法再修改
+            var index;
+            while ( (index = this.tabs.indexOf( tabItem )) >= 0 ) {
+                this.removeByIndex( index );
+            }
         },
 
+        /**
+         * 移除指定索引的标签页
+         * 
+         * @public
+         * @param {number} index 需要移除的标签页的索引
+         */
         removeByIndex: function ( index ) {
-            // TODO
+            if ( this.isDisabled() ) {
+                return;
+            }
+
+            var removed = this.tabs.splice( index, 1 )[ 0 ];
+            if ( !removed ) {
+                return;
+            }
+
+            // 由于实例存储着一份`tabs`对应的DOM元素集合
+            // 这里也需要同步插入更新
+            var tabElement = this.triggers.splice( index, 1 )[ 0 ];
+
+            // 从DOM树移除先
+            tabElement.parentNode.removeChild( tabElement );
+
+            // 如果删的标签在当前激活的标签的前面，
+            // 则当前激活的标签的下标其实改变了，`activeIndex`是要调整的，
+            // 但这种情况下实际激活的还是同一个标签，不用重新渲染
+            if ( index < this.activeIndex ) {
+                this.activeIndex--;
+            }
+            // 如果正好激活的标签被删了，则把激活标签换成当前的后一个，
+            // 如果没有后一个了，则换成最后一个，这需要重新渲染
+            else if ( index === this.activeIndex ) {
+                // 由于可能`activeIndex`没变，因此不能走`setProperties`流程
+                this.activeIndex = Math.min(
+                    this.activeIndex, 
+                    this.tabs.length - 1
+                );
+                activateTab( this, this.activeIndex );
+            }
+
+            // 隐藏对应的关联panel元素
+            if ( removed.panel ) {
+                dom.hide( dom.g( removed.panel ) );
+            }
+
+            /**
+             * @event module:Tab#remove
+             * @param {Object} ev 事件参数对象
+             * @param {string} ev.type 事件类型
+             * @param {Tab} ev.target 触发事件的控件对象
+             * @param {Object} data 当前激活的标签信息
+             * @param {number} data.index 标签索引
+             * @param {Object} data.tab 标签数据项
+             * @param {string} data.tab.title 标签显示标题
+             * @param {string=} data.tab.panel 标签关联的dom容器id
+             */
+            this.emit( 'remove', { index: index, tab: removed } );
         },
 
         /**
@@ -226,6 +429,10 @@ define(function ( require ) {
          * @param {number} index tab标签索引
          */
         select: function ( index ) {
+            if ( this.isDisabled() ) {
+                return;
+            }
+
             if ( index !== this.activeIndex && this.triggers[ index ] ) {
                 this.set( 'activeIndex', index );
             }
@@ -256,11 +463,24 @@ define(function ( require ) {
      * @inner
      * @param {Tab} this Tab控件实例
      * @param {Event} ev 触发事件的事件对象
+     * @fires module:Tab#click
      */
     function clickTab( ev ) {
         var main = this.main;
         var target = ev.target;
         var tabElement = target;
+
+        if ( this.isDisabled() ) {
+            return;
+        }
+
+        /**
+         * @event module:Tab#click
+         * @param {Object} ev 事件参数对象
+         * @param {string} ev.type 事件类型
+         */
+        this.emit( 'click', ev );
+
         while ( tabElement && tabElement.nodeName !== 'LI' ) {
             // 尽量减少回溯深度，最多回溯至主控元素
             if ( main === tabElement ) return;
@@ -294,12 +514,9 @@ define(function ( require ) {
      * @inner
      * @param {Tab} tab Tab控件实例
      * @parma {number} index 待激活的标签页的下标
+     * @fires module:Tab#change
      */
     function activateTab( tab, index ) {
-        if ( tab.onBeforeChange( tab.activeIndex, index ) === false ) {
-            return;
-        }
-
         tab.tabs.forEach(function ( tabItem, i ) {
             var panel = tabItem.panel && dom.g( tabItem.panel );
             if ( panel ) {
@@ -311,7 +528,53 @@ define(function ( require ) {
             ]( tab, 'item-active', tab.triggers[ i ] );
         });
 
+        /**
+         * @event module:Tab#change
+         * @param {Object} ev 事件参数对象
+         * @param {string} ev.type 事件类型
+         * @param {Tab} ev.target 触发事件的控件对象
+         * @param {Object} data 当前激活的标签信息
+         * @param {number} data.index 标签索引
+         * @param {Object} data.tab 标签数据项
+         * @param {string} data.tab.title 标签显示标题
+         * @param {string=} data.tab.panel 标签关联的dom容器id
+         */
         tab.emit( 'change', { index: index, tab: tab.tabs[ index ] } );
+    }
+
+    /**
+     * 刷新构建所有标签页
+     * 主要用于2处:
+     * 1. 首次初始化构建时`initStructrue`
+     * 2. `tabs`数据被外部修改(`set`,`setProperties`)重新赋值后的自动化重绘
+     * 
+     * @inner
+     * @param {Tab} tab 选项卡实例
+     */
+    function rebuildTabs( tab ) {
+        var trigger = dom.g( helper.getId( tab, 'navigator' ) );
+        var parentNode = trigger.parentNode;
+        var referNode = trigger.nextSibling || null;
+
+        // 清空标签DOM内容
+        // 从DOM树临时移除，提高后续DOM操作性能
+        trigger.innerHTML = '';
+        parentNode.removeChild( trigger );
+
+        // 构建所有页卡按钮
+        var triggers = [];
+        var activeIndex = tab.activeIndex;
+        tab.tabs.forEach(function ( tabItem, i ) {
+            var li = createTabElement( tab, tabItem, i === activeIndex );
+            triggers.push( li );
+            trigger.appendChild( li );
+        });
+
+        // 存储选项卡对应元素，减少后续的dom查询次数
+        tab.triggers = triggers;
+
+        // `referNode`值无效时，插入等同于 `appendChild`
+        parentNode.insertBefore( trigger, referNode );
     }
 
 
